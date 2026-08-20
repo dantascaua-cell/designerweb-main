ab0aa80a411ea7fc3f9c1aa83a1e3df104432ab7
dantascaua <dantascaua@escolar.ifr.edu.br>
2026-08-20 13:32:06 -0300

    Atualiza exemplos da aula 19-08

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,11 @@
 alert("Olá Mundo!")
+
+var nasc = 1991;
+let nome = 'Cauã'
+const viva = true
+let altura =1.84
+
+if (viva){
+    let saudacao = "Olá"+ nome +"!";
+    let msg = Altura {{altura}m / Idade  }
+}
