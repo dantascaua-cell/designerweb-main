23c1869a3c2cbbc75b893b47456c191941f057ee
dantascaua <dantascaua@escolar.ifr.edu.br>
2026-08-21 13:31:35 -0300

    Atividade 2 atualizada

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,54 @@
-alert("Olá Mundo!")
+alert("Olá Mundo!");
 
-var nasc = 1991;
-let nome = 'Cauã'
-const viva = true
-let altura =1.84
+var nasc = 2009;
+let nome = "Cauã";
+const viva = true;
+let altura = 1.84;
 
-if (viva){
-    let saudacao = "Olá"+ nome +"!";
-    let msg = Altura {{altura}m / Idade  }
+if (viva) {
+    let saudacao = "Olá " + nome + "!";
+    let msg = `Altura ${altura}m / Idade ${2026 - nasc}`;
+
+    alert(saudacao + "\n" + msg);
+} else {
+    alert("R.I.P");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+0
