31414ea6468f3dcd5a0b5faccda189186fa7bb4f
dantascaua <dantascaua@escolar.ifr.edu.br>
2026-08-26 17:57:03 -0300

    Atividade 4

---
+++ scripts/js_atividade 4/app3.js
@@ -0,0 +1,15 @@
+let nasc = 1991;
+let nome = "Cauã";
+let viva = true;
+let altura = 1.80;
+let sexo = "Masculino";
+let peso = null;
+let im = null;
+
+alert(`Ano de nascimento - tipo: ${typeof(nasc)}`)
+alert(`Nome - tipo: ${typeof(nome)}`)
+alert(`Viva - tipo: ${typeof(viva)}`)
+alert(`Altura - tipo: ${typeof(altura)}`)
+alert(`Sexo - tipo: ${typeof(sexo)}`)
+alert(`Peso - tipo: ${typeof(peso)}`)
+alert(`IM - tipo: ${typeof(im)}`)
