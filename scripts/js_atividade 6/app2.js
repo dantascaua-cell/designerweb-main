7130192102f76a2f2b5f1b96f3bb37be74be62f5
dantascaua <dantascaua@escolar.ifr.edu.br>
2026-09-02 17:58:31 -0300

    Atividade 6 atualizada

---
+++ scripts/js_atividade 6/app2.js
@@ -0,0 +1,20 @@
+let dia = prompt("Digite o dia da semana /nSendo 1 = Domingo - 7 = Sabado");
+dia = Number(dia);
+
+if (dia <=0 || dia > 8){
+    alert("Dia inválido.");
+} else if (dia == 1) {
+    alert("Você escolheu domingo.");
+} else if (dia == 2) {
+    alert("Você escolheu segunda-feira.");
+} else if (dia == 3) {
+    alert("Você escolheu terça-feira.");
+} else if (dia == 4) {
+    alert("Você escolheu quarta-feira.");
+} else if (dia == 5) {
+    alert("Você escolheu quinta-feira.");
+} else if (dia == 6) {
+    alert("Você escolheu sexta-feira.");
+} else if (dia == 7) {
+    alert("Você escolheu sábado.");
+}
